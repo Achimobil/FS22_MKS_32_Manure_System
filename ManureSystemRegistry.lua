c6466600b82d479c861d8d7e4258cf86de86ad3e
Achim Kuntz <achimobil@hotmail.com>
2024-02-14 17:44:23 +0100

    Manure System, Farbwahl und Kapazitätswahl eingebaut

---
+++ ManureSystemRegistry.lua
@@ -0,0 +1,16 @@
+--
+-- ManureSystemRegistry
+--
+-- Author: Wopster (Stijn Wopereis)
+-- Description: Needed to be detected by the Manure System
+-- Name: ManureSystemRegistry
+-- Hide: yes
+--
+-- Copyright (c) Wopster, 2022
+
+---@class ManureSystemRegistry
+ManureSystemRegistry = {}
+
+function ManureSystemRegistry.prerequisitesPresent(specializations)
+    return true
+end
